dfcfd60cde91e21a40ab68b3c8ce444bd2a7ec46
Diego Mosan <diego14290764@itcg.edu.mx>
2018-08-18 09:41:46 -0500

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,21 +2,79 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
+import { Push, PushObject, PushOptions } from '@ionic-native/push';
 import { HomePage } from '../pages/home/home';
+
+
+// New implements
+import { FcmProvider } from '../providers/fcm/fcm';
+import { ToastController, AlertController } from 'ionic-angular';
+import { Subject } from 'rxjs/Subject';
+import { tap } from 'rxjs/operators';
+
+
+
+//End implements section
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   rootPage:any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private push: Push, fcm: FcmProvider, toastCtrl: ToastController) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+      //New implements
+
+        fcm.getToken();
+        fcm.listenToNotifications().pipe(
+          tap(msg => {
+            // const alert = alertCtrl.create({
+            //   title: 'New client',
+            //   subTitle: msg.body,
+            //   buttons: ['Acept', 'Deny']
+            // });
+            // alert.present();
+            const toast = toastCtrl.create({
+              message: msg.body,
+              duration: 3000
+            });
+            toast.present();
+          })
+        )
+        .subscribe()
+      //End new implements
     });
+
+    // this.pushSetup()
   }
+  // pushSetup(){
+  //   const options: PushOptions = {
+  //     android: {
+  //       senderID: '688932327823'
+  //     },
+  //     ios: {
+  //         alert: 'true',
+  //         badge: true,
+  //         sound: 'false'
+  //     },
+  //     // windows: {},
+  //     // browser: {
+  //     //     pushServiceURL: 'http://push.api.phonegap.com/v1/push'
+  //     // }
+  //   };
+     
+  //   const pushObject: PushObject = this.push.init(options);
+    
+    
+  //   pushObject.on('notification').subscribe((notification: any) => console.log('Received a notification', notification));
+    
+  //   pushObject.on('registration').subscribe((registration: any) => console.log('Device registered', registration));
+    
+  //   pushObject.on('error').subscribe(error => console.error('Error with Push plugin', error));
+  // }
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
@@ -6,6 +7,31 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { Push } from '@ionic-native/push';
+
+// =============================================================================
+// New test for push notifications
+// =============================================================================
+
+import { Firebase } from '@ionic-native/firebase';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { FcmProvider } from '../providers/fcm/fcm';
+
+const firebase = {
+  // your firebase web config
+  apiKey: "AIzaSyAmS_9rk7E2IQGEFmJ4AdKqwwo_xR1X5oU",
+  authDomain: "pushnotifications-6211c.firebaseapp.com",
+  databaseURL: "https://pushnotifications-6211c.firebaseio.com",
+  projectId: "pushnotifications-6211c",
+  storageBucket: "pushnotifications-6211c.appspot.com",
+  messagingSenderId: "688932327823"
+ }
+
+// =============================================================================
+// End of the notifications
+// =============================================================================
 
 @NgModule({
   declarations: [
@@ -14,7 +40,12 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    //New implementations
+    AngularFireModule.initializeApp(firebase), 
+    AngularFirestoreModule,
+    HttpClientModule
+    //End new implementations
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -24,6 +55,11 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
+    Push,
+    //New implementations
+    Firebase,
+    FcmProvider,
+    //End new implementations,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
